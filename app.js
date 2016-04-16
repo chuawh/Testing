9d8b10f7d9cf1caa96fbf0c0daaf7e01e5ac936f
chuawh <weihong.chua@voxeolabs.com>
2016-04-16 15:20:04 +0800

    update

--- app.js
+++ app.js
@@ -1,81 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var request = require('request');
 var http = require('http');
 var tropo_webapi = require('tropo-webapi');
-
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//app.use('/', routes);
-
-app.get('/',function(req, res, next) {
-  res.send('App is up and running');  
-});
-
-
-app.post('/users', function (req, res,next) {
-
-    var tropo = new tropo_webapi();
+ 
+var server = http.createServer(function (request, response) {
+    var tropo = new TropoWebAPI();
     tropo.say("Welcome to Tropo!");
     response.end(TropoJSON(tropo));
-});
+}).listen(3000);
 
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-module.exports = app;
