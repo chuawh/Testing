c168fb254cfbcdaa60c95cce077b72c4c6932bc3
chuawh <weihong.chua@voxeolabs.com>
2016-04-19 15:23:01 +0800

    update

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var http = require('http');
 var tropo_webapi = require('tropo-webapi');
  
 var server = http.createServer(function (request, response) {
-    var tropo = new tropo_webapi();
+    var tropo = new TropoWebAPI();
     tropo.say("Welcome to Tropo!");
     response.end(TropoJSON(tropo));
-    console.log(request.body);
-    console.log(response.body);
+    //console.log(request.body);
+    //console.log(response.body);
 }).listen(3000);
 
 console.log("Server is listening port 3000");
