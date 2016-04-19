39854b4c722670ee90a581889b64f40e7e15278a
chuawh <weihong.chua@voxeolabs.com>
2016-04-19 15:13:45 +0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var tropo_webapi = require('tropo-webapi');
  
 var server = http.createServer(function (request, response) {
-    var tropo = new tropo_webapi();
+    var tropo = new TropoWebAPI();
     tropo.say("Welcome to Tropo!");
     response.end(TropoJSON(tropo));
     console.log(request.body);
