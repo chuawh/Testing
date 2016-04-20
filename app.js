eed9f81187e8559ac00b914911cc3f5c59a1b725
chuawh <weihong.chua@voxeolabs.com>
2016-04-20 09:51:38 +0800

    update

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var tropo_webapi = require('tropo-webapi');
 var server = http.createServer(function (request, response) {
     var tropo = new TropoWebAPI();
     tropo.say("Welcome to Singapore!");
+    tropo.call("+6597809414", null, null, null, null, null, "SMS", null, null, null);
+    tropo.say("This message is from Tropo WebAPI in Heroku");
+
     tropo.transfer("whongchu@cisco.com");
     response.end(TropoJSON(tropo));
     //console.log(request);
